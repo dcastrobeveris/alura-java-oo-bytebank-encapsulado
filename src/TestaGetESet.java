1c6911165594b43ceaad70aa8ebd0c747945fd8e
Diego Barcellos <dcastrob@everis.com>
2021-07-21 08:00:12 -0300

    Getters e Setters de referência

--- src/TestaGetESet.java
+++ src/TestaGetESet.java
@@ -5,6 +5,23 @@ public class TestaGetESet {
 		Conta conta = new Conta();
 		conta.setNumero(3377);
 		System.out.println(conta.getNumero());
+	
+	Cliente diego = new Cliente();
+	diego.setNome("Diego Barcellos");
+	
+	conta.setTitular(diego);
+	
+	System.out.println(conta.getTitular().getNome());
+	
+	conta.getTitular().setProfissao("programador");
+	// Agora em duas linhas:
+	Cliente titularDaConta = conta.getTitular();
+	titularDaConta.setProfissao("programador");
+	
+	System.out.println(titularDaConta);
+	System.out.println(diego);
+	System.out.println(conta.getTitular());
+	
 	}
 
 }
