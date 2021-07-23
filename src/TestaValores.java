75de1b03591b17bf0e0140193a5fc5e76a49fa8d
Diego Barcellos <dcastrob@everis.com>
2021-07-23 08:01:53 -0300

    Static

--- src/TestaValores.java
+++ src/TestaValores.java
@@ -4,8 +4,14 @@ public class TestaValores {
 	public static void main(String[] args) {
 		Conta conta = new Conta(3377, 24227);
 		
-		
 		System.out.println(conta.getAgencia());
+		
+		conta.setAgencia(1234);
+		
+		Conta conta2 = new Conta(3377, 25213);
+		Conta conta3 = new Conta(3377, 24313);
+		
+		System.out.println(Conta.getTotal());
 	}
 
 }
